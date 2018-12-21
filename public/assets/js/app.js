a6641cf6cb2e7b08e779f6458b3bbd1cd854cde1
Wake <georgerincon2@gmail.com>
2018-12-21 02:31:43 -0500

    fixed moment sky gradiant time conditions

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,21 +8,19 @@ $(document).ready(function () {
         }).then((res) => {
             location.reload();
         }) */
-    
-        skyChange.getTime().then(skyChange.getAdjust).then(skyChange.changeSky)
-        
-        
+    skyChange.getTime().then(skyChange.getAdjust).then(skyChange.changeSky);
+
 });
 
 
 
 const skyChange = {
-   
-    getTime: () =>{
+
+    getTime: () => {
         let promise = new Promise((resolve, reject) => {
             $.get("/api/time", function (data) {
                 console.log(data);
-                    resolve(data);
+                resolve(data);
             });
         })
 
@@ -30,67 +28,83 @@ const skyChange = {
 
     },
     getAdjust: (res) => {
-        let n = res;
-        let promise = new Promise((resolve,reject) =>{
-            //let n = parseInt(res);
+        //let n = res;
+        let t, m, b;
+        let promise = new Promise((resolve, reject) => {
+            let n = parseInt(res);
+            //et n = res;
+            console.log('this is n', n);
+            console.log(typeof n);
 
-            let t, m, b;
-            if(n == 6){
+            if(n == 1){
+                t = '#000000';
+                m = '#000621';
+                b = '#03135b';
+            }else if(n == 2){
+                t = '#000000';
+                m = '#000621';
+                b = '#2e0447';
+            }else if(n == 3){
+                t = '#000000';
+                m = '#0c0014';
+                b = '#6b4610';
+            }
+            else if (n == 6) {
                 t = '#020023';
                 m = '#230023';
                 b = '#bf510d';
-            }else if([7 , 8].includes(n)){
+            } else if (7 <= n && n <= 8) {
                 t = '#5a6ca8';
                 m = '#e1f6f7';
                 b = '#bf510d';
-            }else if([9 , 10].includes(n)){
+            } else if (9 <= n && n <= 10) {
                 t = '#c4f7ff';
                 m = '#fff5c4';
                 b = '#bf510d';
-            }else if(11 <= n <= 16){
+            } else if (11 <= n && n <= 16) {
                 t = '#0079d6';
                 m = '#7aedff';
                 b = '#a7b8f2';
-            }else if(17 <= n <= 18){
+            } else if (17 <= n && n <= 18) {
                 t = '#c170ff';
                 m = '#fff9e9e';
                 b = '#ffad7a';
-            }else if(n == 19){
+            } else if (n == 19) {
                 t = '#00128c';
                 m = '#8f1dd1';
                 b = '#ea1e8b';
-            }else if(n == 20){
+            } else if (n == 20) {
                 t = '#010123';
                 m = '#460456';
                 b = '#960101';
-            }else{
+            } else {
                 t = '#0d0c38';
                 m = '#010116';
                 b = '#960101';
             }
-            console.log(t);
+            //console.log(t);
 
-           /*  $("#main-box").css({
-                background : `linear-gradient(${t}, ${m}, ${b})`
-            }); */
-            resolve({top: t, mid: m, bottom: b});
+            /*  $("#main-box").css({
+                 background : `linear-gradient(${t}, ${m}, ${b})`
+             }); */
+            resolve({ top: t, mid: m, bottom: b });
         });
 
 
 
         return promise;
     },
-    
-    changeSky: (res) =>{
-        console.log(res);
+
+    changeSky: (res) => {
+        console.log(res.top);
         console.log('Sky gradient re-colored due to time change');
-       let promise = new Promise((resolve, reject) => {
+        let promise = new Promise((resolve, reject) => {
             $("#main-box").css({
-                background : `linear-gradient(${res.top}, ${res.mid}, ${res.bottom})`
+                background: `linear-gradient(${res.top}, ${res.mid}, ${res.bottom})`
             });
             resolve('Sky changed!');
-     });
-        return promise; 
-    } 
+        });
+        return promise;
+    }
 }
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,12 +4,13 @@ const moment = require('moment');
 
 //Dependencies
 const path = require('path');
-
+let time; 
 
 
 module.exports = function (app) {
     app.get("/api/time", function (req, res) {
-        let time = moment().format("HH");
+        time = moment().format("HH");
+        console.log(time);
         res.json(time);
 
         //res.sendFile(path.join(__dirname, "../public/views/index.html"));
